6194e26e066e2dbbe3f5e05eedae7a396c990edc
uju <ujhenry@gmail.com>
2021-08-22 12:44:01 -0700

    'lji'

--- RESUME_BUILDER/settings.py
+++ RESUME_BUILDER/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['hidden-oasis-19764.herokuapp.com', '127.0.0.1']
 
